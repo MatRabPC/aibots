acaa147d1a0619c00d7d526c4a5033e87389dff0
MattRabPC <matthew.rabski@uoit.net>
2018-02-06 12:43:26 -0500

    Week 2 commit

--- testmovement.py
+++ testmovement.py
@@ -1,3 +1,6 @@
+#TODO - make simulation terminate after an event as opposed to time
+#TODO - implement Samina's changes
+#TODO - collision detection with multiple targets drawn?
 from graphics import *
 import simpy
 import random
@@ -10,13 +13,15 @@ DEFINE VARIABLES AND METHODS
 """
 
 
-env = simpy.rt.RealtimeEnvironment(factor=0.07) #movement time
-winHeight = 30 #height of the window
-winWidth = 30 # width of the window
-totalruntime = 200 #amount of steps the system will run for
-timestep = 1 #amount of steps the project will take
+env = simpy.rt.RealtimeEnvironment(factor=0.1) #movement time
+winXLL = 0
+winYLL = 0
+winXUR = 30 #height of the window
+winYUR = 30 # width of the window
+totalruntime = 70 #amount of steps the system will run for
+timestep = 1 #step size
 tgtradius = 0.5
-botradius = 2
+botradius = 1
 
 
 # return either -1, 0 or 1
@@ -49,24 +54,9 @@ CREATE VISUALS
 
 
 #create window
-win = GraphWin('MaSaAn Bots', width = 800, height = 1000) #Create window
-win.setCoords(0,0,winHeight,winWidth) #set window coords by (x lower left, y lower left, x upper right, y upper right)
+win = GraphWin('MaSaAn Bots', width = 800, height = 800) #Create window
+win.setCoords(winXLL, winYLL, winXUR, winYUR) #set window coords by (x lower left, y lower left, x upper right, y upper right)
 win.setBackground('black')
-
-
-#craw a target using circle
-tgt = Circle(Point(randSpawn(winHeight), randSpawn(winWidth)), radius = tgtradius)
-tgt.setFill('white')
-tgt.setOutline('red')
-tgt.draw(win)
-
-
-#draw a bot
-bot = Circle(Point(randSpawn(winHeight), randSpawn(winWidth)), radius = botradius)
-bot.setFill('blue')
-bot.setOutline('white')
-bot.draw(win)
-
 
 
 
@@ -76,15 +66,34 @@ DEFINE ROBOT MOVEMENT AND TARGET COLLECTION
 
 
 def robots(env):
+    i = 1
+    while i <= 3:
+        # draw a target using circle
+        tgt = Circle(Point(randSpawn(winXUR), randSpawn(winYUR)), radius=tgtradius)
+        tgt.setFill('white')
+        tgt.setOutline('blue')
+        print'TARGET %d AT %d, %d' % (i, tgt.getCenter().getX(), tgt.getCenter().getY())
+        tgt.draw(win)
+        i += 1
+
+    # draw a bot
+    bot = Circle(Point(randSpawn(winXUR), randSpawn(winYUR)), radius=botradius)
+    bot.setFill('blue')
+    bot.setOutline('white')
+    bot.draw(win)
+
     while True:
         bot.move(randMov(), randMov()) #assigns random movement to bot, better way to generate?
+        #safeMovBotRandom()
+
         if colDet(bot, tgt) <= 1: #checks colission detection
             print('TARGET FOUND AT %d, %d' %updateCo(bot))
+            tgt.undraw()  # remove target # we should add a globvar or something so we know its been picked up
             yield env.timeout(timestep)
         print('The bot is at %d, %d' %updateCo(bot))
         print('bot is %d away' %colDet(bot, tgt))
-        yield env.timeout(timestep)
 
+        yield env.timeout(timestep)
 
 
 
@@ -102,7 +111,25 @@ env.run(until=totalruntime)#run project for certain amount of time
 win.getMouse()
 
 """Leftover pieces 
+            
+# initialize 5 targets and 1 bot?
+def agentInit(colour):
+    tgtDrawn = 0
+    while tgtDrawn < 5:
+        #draw a target using circle
+        tgt = Circle(Point(randSpawn(winXUR), randSpawn(winYUR)), radius = tgtradius)
+        tgt.setFill('white')
+        tgt.setOutline(colour)
+        tgt.draw(win)
+        tgtDrawn += 1
 
+    #draw a bot
+    bot = Circle(Point(randSpawn(winXUR), randSpawn(winYUR)), radius = botradius)
+    bot.setFill(colour)
+    bot.setOutline('white')
+    bot.draw(win)
+    
+    
 #collision detection on exact point
 if updateBot() == updateTgt():
             print('target found')
@@ -118,9 +145,24 @@ def updateTgt():
     tX = tgt.getCenter().getX()
     tY = tgt.getCenter().getY()
     return tX, tY
-            
-            
-            
-            
-            
+
+
+i = 1
+while i <= 3:
+    #draw a target using circle
+    tgt = Circle(Point(randSpawn(winXUR), randSpawn(winYUR)), radius = tgtradius)
+    tgt.setFill('white')
+    tgt.setOutline('blue')
+    print'TARGET %d AT %d, %d' %(i, tgt.getCenter().getX(), tgt.getCenter().getY())
+    tgt.draw(win)
+    i += 1
+
+#draw a bot
+bot = Circle(Point(randSpawn(winXUR), randSpawn(winYUR)), radius = botradius)
+bot.setFill('blue')
+bot.setOutline('white')
+bot.draw(win)
+botRadar = Circle(Point(bot.getCenter().getX(), bot.getCenter().getY()), radius = botradius + 10)
+botRadar.setOutline('blue')
+botRadar.draw(win)
 """
